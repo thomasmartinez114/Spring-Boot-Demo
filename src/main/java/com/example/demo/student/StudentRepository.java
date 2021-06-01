63a0ac475c6350108d4f43cea6049943beff2f0f
Thomas Martinez <tmartinez114@gmail.com>
2021-05-31 23:03:00 -0400

    Logic of adding new student if email doesnt exisit

--- src/main/java/com/example/demo/student/StudentRepository.java
+++ src/main/java/com/example/demo/student/StudentRepository.java
@@ -3,9 +3,20 @@ package com.example.demo.student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface StudentRepository
         extends JpaRepository<Student, Long> {
+
+    // find user by email
+
+    //    @Query("SELECT s FROM Student s WHERE s.email = ?1")
+
+    // SELECT * FROM student WHERE email = ?
+    Optional<Student> findStudentByEmail(String email);
+
+
 }
 
 // this repository is responsible for data access
